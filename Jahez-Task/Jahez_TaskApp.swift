7369da16b89db7019e9ca4ff3523eaea90ffbbb4
Elsayed Ahmed <N00150@Progbsc.com>
2025-08-18 05:31:43 +0300

    Initial Commit

---
+++ Jahez-Task/Jahez_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jahez_TaskApp.swift
+//  Jahez-Task
+//
+//  Created by Elsayed Ahmed on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Jahez_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
