a6de249e6826c7ae14f9ed6791e29dd3b31315bc
eahmed <60572628+iOSayedAhmed@users.noreply.github.com>
2025-08-19 03:25:20 +0300

    Create Notification.Name+Extension.swift

---
+++ Jahez-Task/Shared/Extensions/Notification.Name+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.Name+Extension.swift
+//  Jahez-Task
+//
+//  Created by Elsayed Ahmed on 18/08/2025.
+//
+
+import Foundation
+
+// MARK: - Notification Names
+extension Notification.Name {
+    // Add Keys here
+}
